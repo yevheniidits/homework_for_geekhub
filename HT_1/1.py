0d88e39305b20bf86683d2ec5a0f2b38134c6255
yevheniidits <73963450+yevheniidits@users.noreply.github.com>
2020-11-08 20:44:05 +0200

    Update 1.py

--- HT_1/1.py
+++ HT_1/1.py
@@ -1,1 +1,10 @@
+""" Write a script which accepts a sequence of comma-separated numbers
+from user and generate a list and a tuple with those numbers """
+
+
+user_data = input("Input some comma-seprated numbers: ")
+list = user_data.split(", ")
+tuple = tuple(list)
+print('List: ', list)
+print('Tuple: ', tuple)
 
